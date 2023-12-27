f21bf72a4958ffe3c9f7d6dc208aefa84de38c42
Izzan Abdul <izzanabdul123@gmail.com>
2023-12-27 14:35:40 +0700

    first commit

---
+++ src/app/register/page.jsx
@@ -0,0 +1,11 @@
+const { default: SignUpCard } = require('@/components/SignUpCard');
+
+const SignUp = () => {
+  return (
+    <main className="max-w-screen-xl h-full mx-auto">
+      <SignUpCard />
+    </main>
+  );
+};
+
+export default SignUp;
